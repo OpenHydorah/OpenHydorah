f8008c1f68590afbd7d607d6afddf73d536e9ec8
Ian T. Jacobsen <iantj92@gmail.com>
2014-04-25 18:57:34 +0100

    Added tests for Dictionary

--- openhydorah/tests/tests_dictionary.c
+++ openhydorah/tests/tests_dictionary.c
@@ -15,22 +15,27 @@ You should have received a copy of the GNU General Public License
 along with OpenHydorah.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <check.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include "dictionary.h"
 
-#include "tests_refptr.c"
-#include "tests_dictionary.c"
+START_TEST (test_dictionary)
+	Dictionary* dict = NULL;
+	int value = 10;
+	char* string = "tests";
 
-int main(int argc, char* argv[])
+	AddToDictionary(&dict, "test1", &value);
+	AddToDictionary(&dict, "test2", string);
+
+	ck_assert_int_eq(value, *(int*)GetFromDict(dict, "test1"));
+	ck_assert_str_eq(string, (char*)GetFromDict(dict, "test2"));
+END_TEST
+
+Suite* dictionary_suite (void)
 {
-	int number_failed;
-	Suite* refptr_s = refptr_suite();
-	Suite* dict_s = dictionary_suite();
-	SRunner* sr = srunner_create(refptr_s);
-	srunner_add_suite(sr, dict_s);
-	srunner_run_all(sr, CK_NORMAL);
-	number_failed = srunner_ntests_failed(sr);
-	srunner_free(sr);
-	return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+	Suite * s = suite_create ("Dictionary");
+
+	TCase* tc_core = tcase_create ("Core");
+	tcase_add_test(tc_core, test_dictionary);
+	suite_add_tcase(s, tc_core);
+
+	return s;
 }
