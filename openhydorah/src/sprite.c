ffb415e3db3d781c1753e2bcc6027e4fdced5d7c
Ian T. Jacobsen <iantj92@gmail.com>
2014-04-25 17:39:32 +0100

    Made sprite.c read JSON

--- openhydorah/src/sprite.c
+++ openhydorah/src/sprite.c
@@ -17,12 +17,11 @@ along with OpenHydorah.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "sprite.h"
 #include <physfs.h>
-#include <libxml/parser.h>
-#include <libxml/tree.h>
 #include <SDL.h>
 #include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
+#include <jansson.h>
 
 Sprite* CreateSprite(void)
 {
@@ -43,82 +42,156 @@ Sprite* CreateSprite(void)
 	return sprite;
 }
 
-void HandleFrame(Frame* frame, xmlNodePtr root)
+int HandleFrame(Frame* frame, json_t* root)
 {
-	xmlNodePtr iter = root;
-
-	while (iter != NULL)
+	if (!json_is_object(root))
 	{
-		if (iter->type == XML_ATTRIBUTE_NODE)
-		{
-		}
-		else if (iter->type == XML_TEXT_NODE)
-		{
-			if (xmlStrEqual(iter->parent->name, "name"))
-			{
-				size_t size = xmlStrlen(iter->content);
-				frame->name = malloc(size);
-				strcpy(frame->name, iter->content);
-			}
-			else if (xmlStrEqual(iter->parent->name, "x"))
-			{
-				frame->rect.x = atoi(iter->content);
-			}
-			else if (xmlStrEqual(iter->parent->name, "y"))
-			{
-				frame->rect.y = atoi(iter->content);
-			}
-			else if (xmlStrEqual(iter->parent->name, "width"))
-			{
-				frame->rect.w = atoi(iter->content);
-			}
-			else if (xmlStrEqual(iter->parent->name, "height"))
-			{
-				frame->rect.h = atoi(iter->content);
-			}
-		}
+		SDL_LogError(
+			SDL_LOG_CATEGORY_APPLICATION,
+			"Frame is invalid JSON type. Expected object.\n"
+		);
 
-		HandleFrame(frame, iter->children);
-
-		iter = iter->next;
+		return 1;
 	}
+
+	json_t* name = NULL;
+	json_t *x,*y,*width,*height;
+
+	name = json_object_get(root, "name");
+	if (!json_is_string(name))
+	{
+		SDL_LogError(
+			SDL_LOG_CATEGORY_APPLICATION,
+			"Could not find name for object.\n"
+		);
+
+		return 1;
+	}
+
+	size_t size = strlen(json_string_value(name));
+	frame->name = malloc(size);
+	strcpy(frame->name, json_string_value(name));
+
+	x = json_object_get(root, "x");
+	if (!json_is_real(x))
+	{
+		SDL_LogWarn(
+				SDL_LOG_CATEGORY_APPLICATION,
+				"Could not get x value for frame '%s'",
+				frame->name
+				);
+	}
+	else
+		frame->rect.x = json_real_value(x);
+
+	y = json_object_get(root, "y");
+	if (!json_is_real(y))
+	{
+		SDL_LogWarn(
+				SDL_LOG_CATEGORY_APPLICATION,
+				"Could not get y value for frame '%s'",
+				frame->name
+				);
+	}
+	else
+		frame->rect.y = json_real_value(y);
+
+	width = json_object_get(root, "width");
+	if (!json_is_real(width))
+	{
+		SDL_LogWarn(
+				SDL_LOG_CATEGORY_APPLICATION,
+				"Could not get width value for frame '%s'",
+				frame->name
+				);
+	}
+	else
+		frame->rect.w = json_real_value(width);
+
+	height = json_object_get(root, "height");
+	if (!json_is_real(height))
+	{
+		SDL_LogWarn(
+				SDL_LOG_CATEGORY_APPLICATION,
+				"Could not get height value for frame '%s'",
+				frame->name
+				);
+	}
+	else
+		frame->rect.h = json_real_value(height);
+
+	return 0;
 }
 
-void FillSpriteWithXML(Sprite* sprite, xmlNodePtr root)
+int FillSprite(Sprite* sprite, json_t* root)
 {
-	xmlNodePtr iter = root;
-	
-	while (iter != NULL)
+	json_t* frames = NULL;
+	json_t* img = NULL;
+	uint32_t i = 0;
+
+	if (!json_is_object(root))
 	{
-		if (iter->type == XML_TEXT_NODE)
-		{
-			if (xmlStrEqual(iter->parent->name, "img") == 1)
-			{
-				sprite->texture = GetTexture(iter->content);
-			}
-		}
-		else if (iter->type == XML_ELEMENT_NODE)
-		{
-			if (xmlStrEqual(iter->name, "frame"))
-			{
-				Frame** frame = &(sprite->frames);
-				while (*frame != NULL)
-					frame = &((*frame)->next);
-				(*frame) = malloc(sizeof(Frame));
-				(*frame)->name = NULL;
-				(*frame)->next = NULL;
-
-				HandleFrame(*frame, iter);
-				sprite->numFrames += 1;
-			}
-			else
-			{
-				FillSpriteWithXML(sprite, iter->children);
-			}
-		}
-
-		iter = iter->next;
+		SDL_LogError(
+				SDL_LOG_CATEGORY_APPLICATION,
+				"Invalid sprite file format\n"
+				);
+		return 1;
 	}
+	
+	img = json_object_get(root, "img");
+	if (!json_is_string(img))
+	{
+		SDL_LogError(
+			SDL_LOG_CATEGORY_APPLICATION,
+			"Could not find 'img' value\n"
+			);
+		json_decref(root);
+		return 1;
+	}
+	sprite->texture = GetTexture(json_string_value(img));
+
+	frames = json_object_get(root, "frames");
+	if (!json_is_array(frames))
+	{
+		SDL_LogError(
+			SDL_LOG_CATEGORY_APPLICATION,
+			"Could not find 'img' value\n"
+			);
+		json_decref(root);
+		return 1;
+	}
+
+	for (i = 0; i < json_array_size(frames); i++)
+	{
+		json_t* frameNode;
+
+		frameNode = json_array_get(frames, i);
+		if (!json_is_object(frameNode))
+		{
+			SDL_LogWarn(
+				SDL_LOG_CATEGORY_APPLICATION,
+				"Frame is invalid JSON type. Expected object.\n"
+			);
+			continue;
+		}
+
+		Frame** frame = &(sprite->frames);
+		while (*frame != NULL)
+			frame = &((*frame)->next);
+		(*frame) = malloc(sizeof(Frame));
+		(*frame)->name = NULL;
+		(*frame)->next = NULL;
+
+		if(HandleFrame(*frame, frameNode) != 0)
+		{
+			free(*frame);
+			*frame = NULL;
+			continue;
+		}
+		sprite->numFrames += 1;
+	}
+
+	return 0;
 }
 
 RefPtr GetSprite(const char* filename)
@@ -127,8 +200,8 @@ RefPtr GetSprite(const char* filename)
 	PHYSFS_File* file = NULL;
 	PHYSFS_sint64 fileLength = 0;
 	uint8_t* buf = NULL;
-	xmlDocPtr doc = NULL;
-	xmlNodePtr rootNode = NULL;
+	json_t* rootNode = NULL;
+	json_error_t error;
 	Sprite* sprite = NULL;
 
 	if (PHYSFS_exists(filename) == 0)
@@ -158,38 +231,33 @@ RefPtr GetSprite(const char* filename)
 	PHYSFS_read(file, buf, 1, fileLength);
 	PHYSFS_close(file);
 
-	doc = xmlReadMemory(buf, fileLength, NULL, "utf-8", 0);
+	rootNode = json_loadb(buf,fileLength,0,&error);
 	free(buf);
 	buf = NULL;
-
-	if (doc == NULL)
-	{
-		SDL_LogError(
-				SDL_LOG_CATEGORY_APPLICATION,
-				"Could not read sprite file '%s'.",
-				filename
-				);
-		return NULL;
-	}
-
-	rootNode = xmlDocGetRootElement(doc);
 
 	if (rootNode == NULL)
 	{
 		SDL_LogError(
 				SDL_LOG_CATEGORY_APPLICATION,
-				"Could not find root node in sprite file '%s'.",
-				filename
+				"JSON error for file '%s' - line: %d - message: %s.",
+				filename, error.line, error.text
 				);
 		return NULL;
 	}
 
 	sprite = CreateSprite();
+	
+	if (sprite == NULL)
+	{
+		return NULL;
+	}
 
-	FillSpriteWithXML(sprite, rootNode);
+	if (FillSprite(sprite, rootNode) != 0)
+	{
+		return NULL;
+	}
 
-	xmlFreeDoc(doc);
-	xmlCleanupParser();
+	json_decref(rootNode);
 
 	refPtr = CreateRefPtr(sprite, DestroySprite);
 
@@ -219,6 +287,9 @@ void DestroySprite(void* sprite)
 
 void DrawSprite(Sprite* sprite, SDL_Renderer* renderer)
 {
+	if (sprite == NULL || sprite->texture == NULL ||
+			sprite->texture->ptr == NULL || sprite->frames == NULL)
+		return;
 	Texture* tex = sprite->texture->ptr;
 
 	SDL_RenderCopy(renderer, tex, &(sprite->frames->rect), NULL);
